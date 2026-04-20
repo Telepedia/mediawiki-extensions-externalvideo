12db6c30ed0d31bccc6757f7eeccf9071d62c1d5
OriginalAuthority <luke@telepedia.net>
2026-04-20 22:12:28 +0100

    bugfix: use a base64 image for the play button rather than SVG

--- includes/ExternalVideoThumbnail.php
+++ includes/ExternalVideoThumbnail.php
@@ -27,37 +27,24 @@ class ExternalVideoThumbnail extends ThumbnailImage {
 	}
 
 	/**
-	 * Get the play button to be added to the wrapped thumnbnail to
-	 * indicate that this is a video
+	 * Get the play button overlay as a data URI image to avoid MediaWiki's
+	 * HTML sanitizer mangling inline SVG content.
 	 * @return string
 	 */
 	private function getPlay(): string {
-		$width = $this->width * 0.3;
-		$height = $this->height * 0.3;
+		$width = (int)( $this->width * 0.3 );
+		$height = (int)( $this->height * 0.3 );
+
 		// phpcs:disable Generic.Files.LineLength.TooLong
-		$play = sprintf( '<svg width="%d" height="%d" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round">
-					<path style="fill:none" d="M0 0h100v100H0z"/>
-					<clipPath id="a">
-						<path d="M0 0h100v100H0z"/>
-					</clipPath>
-					<g clip-path="url(#a)">
-						<path d="M3.125 49.992a46.88 46.88 0 0 0 13.729 33.146 46.88 46.88 0 0 0 66.292 0 46.88 46.88 0 0 0 0-66.292A46.875 46.875 0 0 0 3.125 49.992Z" style="fill:#fff;fill-rule:nonzero;stroke:#fff;stroke-width:6.25px"/>
-						<path d="M37.5 65.054a6.82 6.82 0 0 0 3.999 6.202 6.818 6.818 0 0 0 7.301-1.073L71.875 50 48.8 29.804a6.816 6.816 0 0 0-7.303-1.078 6.82 6.82 0 0 0-3.997 6.207v30.121Z" style="fill:#d9e111;fill-rule:nonzero;stroke:#d9e111;stroke-width:6.25px"/>
-					</g>
-				</svg>',
-				$width,
-				$height
-			);
+		$svg = '<svg width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round"><path style="fill:none" d="M0 0h100v100H0z"/><clipPath id="a"><path d="M0 0h100v100H0z"/></clipPath><g clip-path="url(#a)"><path d="M3.125 49.992a46.88 46.88 0 0 0 13.729 33.146 46.88 46.88 0 0 0 66.292 0 46.88 46.88 0 0 0 0-66.292A46.875 46.875 0 0 0 3.125 49.992Z" style="fill:#fff;fill-rule:nonzero;stroke:#fff;stroke-width:6.25px"/><path d="M37.5 65.054a6.82 6.82 0 0 0 3.999 6.202 6.818 6.818 0 0 0 7.301-1.073L71.875 50 48.8 29.804a6.816 6.816 0 0 0-7.303-1.078 6.82 6.82 0 0 0-3.997 6.207v30.121Z" style="fill:#d9e111;fill-rule:nonzero;stroke:#d9e111;stroke-width:6.25px"/></g></svg>';
 		// phpcs:enable Generic.Files.LineLength.TooLong
 
-		$play = Html::rawElement(
-			'span',
-			[
-				'class' => 'mw-external-video-thumbnail-play',
-			],
-			$play
-		);
+		$src = 'data:image/svg+xml;base64,' . base64_encode( $svg );
 
-		return $play;
+		return Html::rawElement(
+			'span',
+			[ 'class' => 'mw-external-video-thumbnail-play' ],
+			Html::element( 'img', [ 'src' => $src, 'width' => $width, 'height' => $height, 'alt' => '' ] )
+		);
 	}
 }
